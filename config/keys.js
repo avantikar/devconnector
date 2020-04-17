840f3b9c8164d902980578e1f297dda71bb54db4
avantikasathaye <avis45@gmail.com>
2020-04-17 06:21:09 -0700

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://mern01:Password01@devconnectormern-3tgde.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:
+      "mongodb+srv://mern01:Password01@devconnectormern-3tgde.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
+  
